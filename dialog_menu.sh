f83e15f4e5f44ce3981c3dedf77f95b29b6107d3
Matthew Gamble <git@matthewgamble.net>
2016-04-21 20:56:41 +1000

    Add example dialog menu script

---
+++ dialog_menu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cmd=(dialog --menu "Select option:" 22 76 16)
+options=(1 "Option 1"    # any option can be set to default to "on"
+         2 "Option 2"
+         3 "Option 3"
+         4 "Option 4")
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices; do
+	echo $choice
+done
